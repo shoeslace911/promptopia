3f1205cf899334ff9be24e462ce12c108422bf64
Sho <shofujiwara505@gmail.com>
2024-01-24 13:47:36 +0900

    buttons when user is logged in done

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
   return (
-    <div>
-      <h1>Navigator</h1>
-    </div>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image src="/assets/images/logo.svg" alt="Promptopia Logo" width={30} height={30} className="object-contain" />
+        <div className="logo_text">Promptopia</div>
+      </Link>
+      {/* // Desktop Nav */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image src="/assets/images/logo.svg" width={37} height={37} className="rounded-full" alt="profile" />
+            </Link>
+          </div>
+        ) : (
+          <>Log In</>
+        )}
+      </div>
+    </nav>
   );
 };
 

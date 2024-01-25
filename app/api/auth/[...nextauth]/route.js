c26cb122337b918d344b0df1876ac420efb7a128
Sho <shofujiwara505@gmail.com>
2024-01-25 13:39:43 +0900

    next-auth folders setup inside API(new folder)

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: "",
+      clientSecret: "",
+    }),
+  ],
+  async session({ session }) {},
+});

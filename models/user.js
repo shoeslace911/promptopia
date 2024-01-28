fb8862874441b44bb7da311ceeb9c32e6f144c57
Sho <shofujiwara505@gmail.com>
2024-01-28 15:05:06 +0900

    schema

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const { Schema, model, models } = require("mongoose");
+
+const UserSchema = {
+  email: {
+    type: String,
+    unique: [true, "Email already exists"],
+    required: [true, "Email is required"],
+  },
+  username: {
+    type: "String",
+    unique: [true, "Username already exists"],
+    required: [true, "Username is required"],
+    match: [
+      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+      "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
+    ],
+  },
+  image: {
+    type: String,
+  },
+};
+
+const User = model("User", UserSchema);
+
+export default User;

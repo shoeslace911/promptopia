9c96d69d261c6163306af9e7f28d73a98c6958f3
Sho <shofujiwara505@gmail.com>
2024-01-28 15:24:35 +0900

    session

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,8 +10,16 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  //updates everytime so we know who is online
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+    session.user.id = sessionUser._id.toString();
 
-  async session({ session }) {},
+    return session;
+  },
+
   async signIn({ profile }) {
     try {
       await connectToDB();

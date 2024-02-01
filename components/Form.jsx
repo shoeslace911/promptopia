4bf3b3d03aba2d97eec2f313070f1ba778948dd7
Sho <shofujiwara505@gmail.com>
2024-02-01 17:34:16 +0900

    passed the props

--- components/Form.jsx
+++ components/Form.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>
-      <h1>Form me</h1>
-    </div>
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span>{type} post</span>
+      </h1>
+    </section>
   );
 };
+
+// how making the form, passing the props
 
 export default Form;

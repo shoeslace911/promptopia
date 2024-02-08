50036166f040e2a9dea7e013965ad505433ef519
Sho <shofujiwara505@gmail.com>
2024-02-08 16:12:16 +0900

    problem with the image not being able to find data.creator.image

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+"use client";
 
-const PromptCard = ({ key, post, handleTagClick }) => {
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const PromptCard = ({ key, post, handleTagClick, handleEdit, handleDelete }) => {
   return (
-    <div>
-      {console.log("Prompt Card", post)}
-      {post.map((data) => (
-        <h1>{data.prompt}</h1>
-      ))}
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <Image
+          // src={post.creator.image}
+          alt="user_image"
+          width={40}
+          height={40}
+          className="rounded-full object-contain"
+        />
+      </div>
     </div>
   );
 };

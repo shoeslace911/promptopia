f17da806eedde07ee10bcd110a37e1426d1ac426
Sho <shofujiwara505@gmail.com>
2024-02-13 13:02:41 +0900

    routing done??? well see because I will make the profile page

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,14 @@
+const { default: Prompt } = require("@models/prompt");
+const { connectToDB } = require("@utils/database");
+
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    const prompts = await Prompt.find({ creator: params.id }).populate("creator");
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};

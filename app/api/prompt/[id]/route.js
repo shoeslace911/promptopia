8d4c1cd479fba50d00f0651fff5b9b7bea5affee
Sho <shofujiwara505@gmail.com>
2024-02-15 14:41:12 +0900

    made the get route for edit

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,2 +1,15 @@
-// here is the route to be able to get the data from the database and
-// update or delete those in the components of Profile
+const { default: Prompt } = require("@models/prompt");
+const { connectToDB } = require("@utils/database");
+
+export const GET = async ({ params }) => {
+  try {
+    await connectToDB();
+
+    const prompt = await Prompt.findByID(params.id).populate("creator");
+    if (!prompt) return new Response("Prompt not found", { status: 404 });
+
+    return new Response(JSON.stringify(prompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};

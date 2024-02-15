b810d46379fe430ce403630c8e403bab049cb7e6
Sho <shofujiwara505@gmail.com>
2024-02-15 16:31:42 +0900

    routing for edit com@plete

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,7 +1,7 @@
 const { default: Prompt } = require("@models/prompt");
 const { connectToDB } = require("@utils/database");
 
-export const GET = async ({ params }) => {
+export const GET = async (request, { params }) => {
   try {
     await connectToDB();
 
@@ -11,5 +11,29 @@ export const GET = async ({ params }) => {
     return new Response(JSON.stringify(prompt), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
+
+export const PATCH = async (request, { params }) => {
+  // passing new stuff through request
+  const { prompt, tag } = await request.json();
+
+  try {
+    await connectToDB();
+    // find the specific prompt via ID
+    const existingPrompt = await Prompt.findByID(params.id);
+
+    if (!existingPrompt) return new Response("Prompt not found", { status: 404 });
+    // overriding starts here
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    // save
+    await existingPrompt.save();
+
+    // return a readable version of the data we collected
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+    new Response("Cannot edit file", { status: 505 });
   }
 };
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,13 +5,13 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React, { useState, useEffect } from "react";
 
-const handleEdit = () => {};
-const handleDelete = async () => {};
-
 export default function MyProfile() {
   const [posts, setPosts] = useState([]);
   const { data: session } = useSession();
-  console.log(session?.user.id);
+
+  const handleEdit = (post) => {};
+  const handleDelete = async () => {};
+
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await fetch(`/api/users/${session?.user.id}/posts`);
@@ -21,7 +21,6 @@ export default function MyProfile() {
     };
 
     if (session?.user.id) fetchPosts();
-    console.log(fetchPosts());
   }, [session?.user.id]);
   {
   }
